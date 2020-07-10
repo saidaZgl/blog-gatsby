aa7a67bf9a6f01a4cc46d1018132ada4655cb512
Saida Zghal <zghal.saida@orange.fr>
2020-07-10 15:51:10 +0200

    netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: { name: `blog`, path: `${__dirname}/blog/` },
